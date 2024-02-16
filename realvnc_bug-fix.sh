5f8cacbf8bb5ff42e9f9d381da33398e705877bc
Glaidson Verzeletti <verzeletti@gmail.com>
2024-02-16 11:27:12 -0300

    Update realvnc_bug-fix.sh

--- realvnc_bug-fix.sh
+++ realvnc_bug-fix.sh
@@ -3,13 +3,12 @@
 # Script File Download
 #wget -nv https://github.com/verzeletti/raspberrypi/raw/main/realvnc_bug-fix.sh
 
-echo "Logging with root";
-sudo su -
+echo "Logging with root and move script" ;
 
 # error while loading shared libraries: libbcm_host.so: cannot open shared object file
 echo "Update and install packages";
-apt-get update
-apt-get install --reinstall libraspberrypi0 libraspberrypi-dev libraspberrypi-doc libraspberrypi-bin
+sudo apt-get update
+sudo apt-get install --reinstall libraspberrypi0 libraspberrypi-dev libraspberrypi-doc libraspberrypi-bin
 
 # error while loading shared libraries: /lib/arm-linux-gnueabihf/libmmal.so: file too short
 echo "Download and install dependencies";
@@ -17,8 +16,8 @@ files=( libmmal.so libmmal_core.so libmmal_components.so libmmal_util.so libmmal
 for i in "${files[@]}"
 do
         #wget -nv https://github.com/raspberrypi/firmware/raw/master/opt/vc/lib/$i -O /usr/lib/arm-linux-gnueabihf/$i
-        wget -nv https://github.com/verzeletti/raspberrypi/raw/main/firmware/$i -O /usr/lib/arm-linux-gnueabihf/$i
+        sudo wget -nv https://github.com/verzeletti/raspberrypi/raw/main/firmware/$i -O /usr/lib/arm-linux-gnueabihf/$i
 done
 
 echo "Restarting services";
-systemctl restart vncserver-x11-serviced.service; systemctl status vncserver-x11-serviced.service
+sudo systemctl restart vncserver-x11-serviced.service; sudo systemctl status vncserver-x11-serviced.service
