8618fba5dd31cfb9fd8a1517cb04b3b812259929
Glaidson Verzeletti <verzeletti@gmail.com>
2024-02-16 11:20:37 -0300

    Update realvnc_bug-fix.sh

--- realvnc_bug-fix.sh
+++ realvnc_bug-fix.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# error while loading shared libraries: libbcm_host.so: cannot open shared object file
+echo Update and install packages
+sudo apt-get update
+sudo apt-get install --reinstall libraspberrypi0 libraspberrypi-dev libraspberrypi-doc libraspberrypi-bin
+
+# error while loading shared libraries: /lib/arm-linux-gnueabihf/libmmal.so: file too short
 echo Download and install dependencies
 files=( libmmal.so libmmal_core.so libmmal_components.so libmmal_util.so libmmal_vc_client.so libvcsm.so libcontainers.so )
 for i in "${files[@]}"
